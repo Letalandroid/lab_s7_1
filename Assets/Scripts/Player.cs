28439f69b27a245425cac6dbf0d1d1e219f1abce
UCV-PIURA <UCV-PIURA@ucvvirtual.edu.pe>
2024-10-14 10:12:33 -0500

    Finished

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+// A very simplistic car driving on the x-z plane.
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+    private Animator anim;
+
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+        anim.SetFloat("MovX", translation);
+        anim.SetFloat("MovY", rotation);
     }
 }
